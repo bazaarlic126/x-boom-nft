a9836d46388dde3aea19e95246fe48a62c565ff3
bazaarlic126 <68413949+bazaarlic126@users.noreply.github.com>
2025-06-07 21:23:34 +0500

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,12 +1,4 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
 const PORT = process.env.PORT || 3000;
-
-// Отдаём статические файлы из папки public
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
